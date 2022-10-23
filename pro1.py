ac9462e2a3c99abead1d7e193e820ef81ef6861a
localuser <localuser@gmail.com>
2022-10-23 01:44:00 +0000

    pro1.py modified

---
+++ pro1.py
@@ -0,0 +1,12 @@
+
+print (" ========= Welcome to my Repository, where I store valuable information & data ========== ")
+
+invinum = input("What is your invitation number")
+
+if invinum == "123":
+    print  ("Congrats, You are the very first participant!!!")
+
+else:
+    print ("Wrong Invitation Number :(..... Exiting.....")
+
+
